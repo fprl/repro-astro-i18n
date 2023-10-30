2e867eacbf7c15d982e01ecad1072d315eada8e4
Franco Pablo Romano Losada <francoromano1994@gmail.com>
2023-10-30 17:19:07 +0100

    test

--- src/pages/luxury-villa-rental/_components/_search-store.ts
+++ src/pages/luxury-villa-rental/_components/_search-store.ts
@@ -1,7 +1,7 @@
 // stores/searchParams.ts
 import { navigate } from 'astro:transitions/client';
 
-export function changeSearchParam(key: 'filters[persons]' | 'filters[bedrooms]', value: string) {
+export function changeSearchParam(key: 'filters[persons]' | 'filters[bedrooms]' | 'destination', value: string) {
   // searchParams.open({ ...searchParams.get(), [key]: value })
   const params = new URLSearchParams(window.location.search);
   params.set(key, value);
---
+++ src/pages/luxury-villa-rental/_components/filters.destination.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { changeSearchParam } from './_search-store';
+
+  export let destination: 'ibiza' | 'mallorca' = 'ibiza';
+  !destination && (destination = 'ibiza');
+
+  const destinations = ['ibiza', 'mallorca'];
+</script>
+
+<div class="p-2 border">
+  <label for="destination">Destination: {destination}</label>
+  <div class="flex gap-x-2">
+    <!-- each for destinations -->
+    {#each destinations as option (option)}
+      <button
+        class="border px-2 py-1 hover:bg-gray-200"
+        class:bg-gray-100={destination === option}
+        on:click={() => changeSearchParam('destination', option)}
+      >
+        {option}
+      </button>
+    {/each}
+  </div>
+</div>
